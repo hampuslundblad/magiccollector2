95616785c95c7fb0973d917368834f493b1781f8
hampuslundblad <hampus.lundblad@hotmail.se>
2023-12-25 17:47:27 +0100

    add key to search item

--- magiccollector/src/components/ScryfallSearchInput.tsx
+++ magiccollector/src/components/ScryfallSearchInput.tsx
@@ -1,38 +1,35 @@
 import { LoaderIcon } from "lucide-react";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Input } from "./ui/input";
 import { debounce } from "lodash";
-import  cardNames from "../lib/cardNames.json"
+import cardNames from "../lib/cardNames.json";
 
 const ScryfallSearchInput = () => {
   const [searchValue, setSearchValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [filteredValues, setFilteredValues] = useState<string[]>([])
+  const [filteredValues, setFilteredValues] = useState<string[]>([]);
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setSearchValue(value);
-    debouncedSearch();
     if (value.trim() === "") {
       setFilteredValues([]);
       return;
-    }  
-    else if (value.length <= 2){
+    } else if (value.length <= 2) {
       return;
-    }
-    else {
-      debouncedSearch()
+    } else {
+      debouncedFilter();
     }
   };
 
-  const filterCards = () => { 
-      if(searchValue.trim() !== ""){
-        setFilteredValues(cardNames.filter((x) => x.toLowerCase().includes(searchValue.toLowerCase())));
-      }
-  };
+  const filterCards = useCallback(() => { 
+    setFilteredValues(
+      cardNames.filter((x) =>
+        x.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    );
+  },[searchValue])
 
-  const debouncedSearch = debounce(filterCards, 300); // Debounce the handleSearch function
-
-  
+  const debouncedFilter = debounce(filterCards, 300); // Debounce the handleSearch function
 
   return (
     <div>
@@ -44,7 +41,9 @@ const ScryfallSearchInput = () => {
       />
       {isLoading && <LoaderIcon />}
       <div className="w-fill h-fit mt-2 divde-y divide-solid">
-        {filteredValues.slice(0,10).map((x) => <div> {x} </div>)}
+        {filteredValues.slice(0, 10).map((x, index) => (
+          <div key={index}> {x} </div>
+        ))}
       </div>
     </div>
   );
